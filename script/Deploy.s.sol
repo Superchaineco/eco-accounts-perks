9e94d3b7453752333585b9e0748ed83c4eb65128
anotherdev <joseluismanco37@gmail.com>
2025-10-21 19:59:07 -0500

    feat: add deployment script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {EcoAccountsPerks} from "../src/EcoAccountsPerks.sol";
+
+contract DeployEcoAccountsPerks is Script {
+    function run() external {
+        address initialOwner = vm.envAddress("INITIAL_OWNER");
+        address ecoAccountsBadgesAddress = vm.envAddress("ECO_ACCOUNTS_BADGES_ADDRESS");
+
+        vm.startBroadcast();
+
+        EcoAccountsPerks perks = new EcoAccountsPerks(initialOwner, ecoAccountsBadgesAddress);
+
+        vm.stopBroadcast();
+
+        console.log("EcoAccountsPerks deployed on: ", address(perks));
+    }
+}
